44d8d8f6fe76f32ab61d25b76ddf59c7b1fa91a7
Hiro Hito <etienne.ogez@gmail.com>
2023-01-27 15:45:40 +0100

    On s'en fou

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 
 let
-  masuperlib-src = fetchTarball https://github.com/Hiro-Hito/masuperlib/releases/tag/v0.1;
+  masuperlib-src = fetchTarball https://github.com/Hiro-Hito/masuperlib/archive/refs/tags/v0.1.tar.gz;
 
   masuperlib = callPackage masuperlib-src {};
 
